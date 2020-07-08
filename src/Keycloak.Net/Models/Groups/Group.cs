9b1b65f8d3fa9bb1e60658349a98e5b467e54e63
Slavko Salontaji <s.salontaji@vegaitsourcing.rs>
2020-07-08 16:20:22 +0200

    Added are more groups fields (relam and client roles, attributes, subgroups)

--- src/Keycloak.Net/Models/Groups/Group.cs
+++ src/Keycloak.Net/Models/Groups/Group.cs
@@ -1,4 +1,7 @@
-﻿using Newtonsoft.Json;
+﻿using Keycloak.Net.Models.Roles;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 
 namespace Keycloak.Net.Models.Groups
 {
@@ -10,5 +13,13 @@ namespace Keycloak.Net.Models.Groups
         public string Name { get; set; }
         [JsonProperty("path")]
         public string Path { get; set; }
+        [JsonProperty("realmRoles")]
+        public IEnumerable<Role> RealmRoles { get; set; }
+        [JsonProperty("clientRoles")]
+        public IEnumerable<Role> ClientRoles { get; set; }
+        [JsonProperty("attributes")]
+        public IEnumerable<Attribute> Attributes { get; set; }
+        [JsonProperty("subGroups")] 
+        public IEnumerable<Group> Subgroups {get; set;}
     }
 }
