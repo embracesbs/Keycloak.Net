d5e09508d30336f434524e9fb79ee46c14cb90e9
Slavko Salontaji <s.salontaji@vegaitsourcing.rs>
2020-07-09 09:42:59 +0200

    Improve group structure

--- src/Keycloak.Net/Models/Groups/Group.cs
+++ src/Keycloak.Net/Models/Groups/Group.cs
@@ -1,11 +1,10 @@
 ﻿using Keycloak.Net.Models.Roles;
 using Newtonsoft.Json;
-using System;
 using System.Collections.Generic;
 
 namespace Keycloak.Net.Models.Groups
 {
-    public class Group
+    public class Simplegroup
     {
         [JsonProperty("id")]
         public string Id { get; set; }
@@ -13,13 +12,17 @@ namespace Keycloak.Net.Models.Groups
         public string Name { get; set; }
         [JsonProperty("path")]
         public string Path { get; set; }
+        [JsonProperty("subGroups")]
+        public IEnumerable<Simplegroup> Subgroups { get; set; }
+    }
+
+    public class Group : Simplegroup
+    {
         [JsonProperty("realmRoles")]
         public IEnumerable<Role> RealmRoles { get; set; }
         [JsonProperty("clientRoles")]
-        public IEnumerable<Role> ClientRoles { get; set; }
+        public IDictionary<string, string> ClientRoles { get; set; }
         [JsonProperty("attributes")]
-        public IEnumerable<Attribute> Attributes { get; set; }
-        [JsonProperty("subGroups")] 
-        public IEnumerable<Group> Subgroups {get; set;}
+        public IDictionary<string, string> Attributes { get; set; }
     }
 }
