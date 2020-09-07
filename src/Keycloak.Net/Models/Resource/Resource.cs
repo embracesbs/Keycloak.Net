24b9b5c2aa4beefe549c485c061520ef3cded494
Slavko Salontaji <s.salontaji@vegaitsourcing.rs>
2020-09-07 11:26:29 +0200

    Initial implementation

---
+++ src/Keycloak.Net/Models/Resource/Resource.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Keycloak.Net.Models.Resource
+{
+    public class Resource
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        [JsonProperty("scopes")]
+        public IEnumerable<Scope> Scopes { get; set; }
+        [JsonProperty("attributes")]
+        public Dictionary<string, string> Attributes { get; set; }
+        [JsonProperty("uris")]
+        public IEnumerable<string> Uris { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("ownerManagedAccess")]
+        public bool? OwnerManagedAccess { get; set; }
+        [JsonProperty("displayName")]
+        public string DisplayName { get; set; }
+        [JsonProperty("type")]
+        public string Type { get; set; } 
+    }
+}
