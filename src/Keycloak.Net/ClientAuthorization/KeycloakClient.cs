55cc6c55aabcf72cc1bb13104f02b1128f5c9204
Slavko Salontaji <s.salontaji@vegaitsourcing.rs>
2021-02-24 09:30:16 +0100

    Refactor Policy related code toward generic helper methods Implement GroupPolicy

--- src/Keycloak.Net/ClientAuthorization/KeycloakClient.cs
+++ src/Keycloak.Net/ClientAuthorization/KeycloakClient.cs
@@ -98,29 +98,12 @@ namespace Keycloak.Net
                 .GetJsonAsync<IEnumerable<AuthorizationResource>>()
                 .ConfigureAwait(false);
         }
-        #endregion 
+        #endregion
 
         #region Policy
-        public async Task<RolePolicy> CreateRolePolicyAsync(string realm, string clientId, RolePolicy policy)
-        {
-                var response = await GetBaseUrl(realm)
-                    .AppendPathSegment($"/admin/realms/{realm}/clients/{clientId}/authz/resource-server/policy")
-                    .AppendPathSegment(policy.Type == PolicyType.Role ? "/role" : string.Empty)
-                    .PostJsonAsync(policy)
-                    .ReceiveJson<RolePolicy>()
-                    .ConfigureAwait(false);
-                return response;
-        }
 
-        public async Task<RolePolicy> GetRolePolicyByIdAsync(string realm, string clientId, PolicyType policyType, string rolePolicyId) => await GetBaseUrl(realm)
-            .AppendPathSegment($"/admin/realms/{realm}/clients/{clientId}/authz/resource-server/policy")
-            .AppendPathSegment(policyType == PolicyType.Role ? "/role" : string.Empty)
-            .AppendPathSegment($"/{rolePolicyId}")
-            .GetJsonAsync<RolePolicy>()
-            .ConfigureAwait(false);
-
-        public async Task<IEnumerable<Policy>> GetAuthorizationPoliciesAsync(string realm, string clientId, 
-            int? first = null, int? max = null, 
+        public async Task<IEnumerable<Policy>> GetAuthorizationPoliciesAsync(string realm, string clientId,
+            int? first = null, int? max = null,
             string name = null, string resource = null,
             string scope = null, bool? permission = null)
         {
@@ -141,10 +124,91 @@ namespace Keycloak.Net
                 .ConfigureAwait(false);
         }
 
+        public async Task<RolePolicy> CreateRolePolicyAsync(string realm, string clientId, RolePolicy policy)
+        {
+            return await CreatePolicyAsync<RolePolicy>(realm, clientId, policy, PolicyType.Role, "role").ConfigureAwait(false);
+        }
+
+        public async Task<RolePolicy> GetRolePolicyByIdAsync(string realm, string clientId, PolicyType policyType, string rolePolicyId)
+        {
+            return await GetPolicyByIdAsync<RolePolicy>(realm, clientId, policyType, rolePolicyId, PolicyType.Role, "role").ConfigureAwait(false);
+        }
+
         public async Task<IEnumerable<RolePolicy>> GetRolePoliciesAsync(string realm, string clientId, 
             int? first = null, int? max = null, 
             string name = null, string resource = null,
             string scope = null, bool? permission = null)
+        {
+            return await GetPoliciesAsync<RolePolicy>(realm, clientId, first, max, name, resource, scope, permission, "role")
+                .ConfigureAwait(false);
+        }
+
+        public async Task<bool> UpdateRolePolicyAsync(string realm, string clientId, RolePolicy policy)
+        {
+            return await UpdatePolicy(realm, clientId, policy, PolicyType.Role, "role")
+                .ConfigureAwait(false);
+        }
+
+        public async Task<bool> DeleteRolePolicyAsync(string realm, string clientId, PolicyType policyType, string rolePolicyId)
+        {
+            return await DeletePolicyAsync(realm, clientId, policyType, rolePolicyId, PolicyType.Role, "role")
+                .ConfigureAwait(false);
+        }
+
+        public async Task<GroupPolicy> CreateGroupPolicyAsync(string realm, string clientId, GroupPolicy policy)
+        {
+            return await CreatePolicyAsync<GroupPolicy>(realm, clientId, policy, PolicyType.Group, "group").ConfigureAwait(false);
+        }
+
+        public async Task<GroupPolicy> GetGroupPolicyByIdAsync(string realm, string clientId, PolicyType policyType, string groupPolicyId)
+        {
+            return await GetPolicyByIdAsync<GroupPolicy>(realm, clientId, policyType, groupPolicyId, PolicyType.Group, "group").ConfigureAwait(false);
+        }
+
+        public async Task<IEnumerable<GroupPolicy>> GetGroupPoliciesAsync(string realm, string clientId,
+            int? first = null, int? max = null,
+            string name = null, string resource = null,
+            string scope = null, bool? permission = null)
+        {
+            return await GetPoliciesAsync<GroupPolicy>(realm, clientId, first, max, name, resource, scope, permission, "group")
+                .ConfigureAwait(false);
+        }
+
+        public async Task<bool> UpdateGroupPolicyAsync(string realm, string clientId, GroupPolicy policy)
+        {
+            return await UpdatePolicy(realm, clientId, policy, PolicyType.Group, "group")
+                .ConfigureAwait(false);
+        }
+
+        public async Task<bool> DeleteGroupPolicyAsync(string realm, string clientId, PolicyType policyType, string groupPolicyId)
+        {
+            return await DeletePolicyAsync(realm, clientId, policyType, groupPolicyId, PolicyType.Group, "group")
+                .ConfigureAwait(false);
+        }
+
+        private async Task<T> GetPolicyByIdAsync<T>(string realm, string clientId, PolicyType policyType, string policyId, PolicyType expectedPolicyType, string path)
+        {
+            return await GetBaseUrl(realm)
+              .AppendPathSegment($"/admin/realms/{realm}/clients/{clientId}/authz/resource-server/policy")
+              .AppendPathSegment(policyType == expectedPolicyType ? $"/{path}" : string.Empty)
+              .AppendPathSegment($"/{policyId}")
+              .GetJsonAsync<T>()
+              .ConfigureAwait(false);
+        }
+
+        private async Task<T> CreatePolicyAsync<T>(string realm, string clientId, T policy, PolicyType policyType, string path)
+            where T : PolicyBase
+        {
+            return await GetBaseUrl(realm)
+                .AppendPathSegment($"/admin/realms/{realm}/clients/{clientId}/authz/resource-server/policy")
+                .AppendPathSegment(policy.Type == policyType ? $"/{path}" : string.Empty)
+                .PostJsonAsync(policy)
+                .ReceiveJson<T>()
+                .ConfigureAwait(false);
+        }
+
+        private async Task<IEnumerable<T>> GetPoliciesAsync<T>(string realm, string clientId, int? first, int? max, string name, string resource, string scope, bool? permission, string typeName)
+            where T : PolicyBase
         {
             var queryParams = new Dictionary<string, object>
             {
@@ -157,29 +221,35 @@ namespace Keycloak.Net
             };
 
             return await GetBaseUrl(realm)
-                .AppendPathSegment($"/admin/realms/{realm}/clients/{clientId}/authz/resource-server/policy/role")
+                .AppendPathSegment($"/admin/realms/{realm}/clients/{clientId}/authz/resource-server/policy/{typeName}")
                 .SetQueryParams(queryParams)
-                .GetJsonAsync<IEnumerable<RolePolicy>>()
+                .GetJsonAsync<IEnumerable<T>>()
                 .ConfigureAwait(false);
         }
 
-        public async Task<bool> UpdateRolePolicyAsync(string realm, string clientId, RolePolicy policy)
+        private async Task<bool> UpdatePolicy<T>(string realm, string clientId, T policy, PolicyType type, string path)
+            where T: PolicyBase
         {
             var response = await GetBaseUrl(realm)
                 .AppendPathSegment($"/admin/realms/{realm}/clients/{clientId}/authz/resource-server/policy")
-                .AppendPathSegment(policy.Type == PolicyType.Role ? "/role" : string.Empty)
+                .AppendPathSegment(policy.Type == type ? $"/{path}" : string.Empty)
                 .AppendPathSegment($"/{policy.Id}")
                 .PutJsonAsync(policy)
                 .ConfigureAwait(false);
             return response.IsSuccessStatusCode;
         }
 
-        public async Task<bool> DeleteRolePolicyAsync(string realm, string clientId, PolicyType policyType, string rolePolicyId)
+        private async Task<bool> DeletePolicyAsync(
+            string realm, 
+            string clientId, 
+            PolicyType policyType, 
+            string policyId, 
+            PolicyType type, string path)
         {
             var response = await GetBaseUrl(realm)
                 .AppendPathSegment($"/admin/realms/{realm}/clients/{clientId}/authz/resource-server/policy")
-                .AppendPathSegment(policyType == PolicyType.Role ? "/role" : string.Empty)
-                .AppendPathSegment($"/{rolePolicyId}")
+                .AppendPathSegment(policyType == type ? $"/{path}" : string.Empty)
+                .AppendPathSegment($"/{policyId}")
                 .DeleteAsync()
                 .ConfigureAwait(false);
             return response.IsSuccessStatusCode;
--- src/Keycloak.Net/Models/Clients/Policy.cs
+++ src/Keycloak.Net/Models/Clients/Policy.cs
@@ -5,7 +5,7 @@ using Newtonsoft.Json;
 
 namespace Keycloak.Net.Models.Clients
 {
-    public class Policy
+    public class PolicyBase
     {
         [JsonProperty("id")]
         public string Id { get; set; }
@@ -20,37 +20,38 @@ namespace Keycloak.Net.Models.Clients
         public PolicyType Type { get; set; }
 
         [JsonConverter(typeof(PolicyDecisionLogicConverter))]
-        public PolicyDecisionLogic Logic { get; set; } 
+        public PolicyDecisionLogic Logic { get; set; }
 
         [JsonConverter(typeof(DecisionStrategiesConverter))]
         public DecisionStrategy DecisionStrategy { get; set; }
+    }
 
+    public class Policy: PolicyBase
+    {
         [JsonProperty("config")]
         public PolicyConfig Config { get; set; }
     }
 
-    public class RolePolicy
+    public class RolePolicy: PolicyBase
     {
-        [JsonProperty("id")]
-        public string Id { get; set; }
-
-        [JsonProperty("name")]
-        public string Name { get; set; }
-
-        [JsonProperty("description")]
-        public string Description { get; set; }
-
-        [JsonConverter(typeof(PolicyTypeConverter))]
-        public PolicyType Type { get; set; } = PolicyType.Role;
-
-        [JsonConverter(typeof(PolicyDecisionLogicConverter))]
-        public PolicyDecisionLogic Logic { get; set; } 
-
-        [JsonConverter(typeof(DecisionStrategiesConverter))]
-        public DecisionStrategy DecisionStrategy { get; set; }
+        public RolePolicy()
+        {
+            Type = PolicyType.Role;
+        }
 
         [JsonProperty("roles")]
         public IEnumerable<RoleConfig> RoleConfigs { get; set; }
+    }
+
+    public class GroupPolicy: PolicyBase
+    {
+        public GroupPolicy()
+        {
+            Type = PolicyType.Group;
+        }
+
+        [JsonProperty("groups")]
+        public IEnumerable<GroupConfig> GroupConfigs { get; set; }
     }
 
     public class RoleConfig
@@ -60,6 +61,18 @@ namespace Keycloak.Net.Models.Clients
 
         [JsonProperty("required")]
         public bool Required { get; set; }
+    }
+
+    public class GroupConfig
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("extendChildren")]
+        public bool ExtendChildren { get; set; }
+
+        [JsonProperty("path")]
+        public string Path { get; set; }
     }
 
     public enum PolicyType
